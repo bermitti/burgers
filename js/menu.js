2ead41515cd7d37daa29d7c96841da1109105304
bermitti <bermitti@gmail.com>
2017-08-20 10:43:25 +0300

    js popup menu;

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,21 @@
 
 // полноэкранное меню -- ВСПЛЫВАШКА ДЛЯ ТЕЛЕФОНА --
 // класс .active в burger-menu.scss
-$(document).ready(function() {
-	var menuOn = $('.intro-menu-link'),  //значение кнопки открытия полноэкранного меню 
-		menuOff = $('.humburger-menu__close'),  //значение кнопки закрытия полноэкранного меню 
-		menu = $('.humburger-menu');  //div-скрытая секция полноэкранного меню , в десктоп разрешении
 
-$(menuOn).on('click',function(e){
-    e.preventDefault();
-    $(menu).toggleClass('active');
-});
-$(menuOff).on('click',function(e){
-    e.preventDefault();
-    $(menu).toggleClass('active');
-});
+//значение кнопки открытия полноэкранного меню
+//значение кнопки закрытия полноэкранного меню 
+//div-скрытая секция полноэкранного меню , в десктоп разрешении
+$(document).ready(function(){
+	var menuOn = $('.intro-menu-link'),   
+		menuOff = $('.humburger-menu__close'),  
+		menu = $('.humburger-menu');  
+
+        $(menuOn).on('click',function(e){
+            e.preventDefault();
+            $(menu).toggleClass('active');
+        });
+        $(menuOff).on('click',function(e){
+            e.preventDefault();
+            $(menu).toggleClass('active');
+        });
+    });
