28176dcfe2001c481b6ea685530c6ce30eb4e37d
bermitti <bermitti@gmail.com>
2017-08-22 11:58:22 +0300

    slider n2

--- js/app.js
+++ js/app.js
@@ -74,6 +74,11 @@ $(document).ready(function(){
     //     });
     // });
     
-    $('.bxslider').bxSlider();
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        auto: true,
+        autoControls: true,
+        pause: 2000
+    });
 });
 
