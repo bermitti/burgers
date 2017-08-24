60441f79b017e37725d2d5b525c8088ce57fb745
bermitti <bermitti@gmail.com>
2017-08-24 22:35:13 +0300

    scroll

--- js/myOnePageScroll.js
+++ js/myOnePageScroll.js
@@ -1,51 +1,28 @@
 
-//one page scroll
-    // $('.wrapper').on('wheel',function(e){
-    //     var deltaY = e.originalEvent.deltaY,
-    //         section = $('.section'),
-    //         activeSect = section.filter('.activeSection'),
-    //         nextSection = activeSect.next(),
-    //         display = $('.main-content');
-    //         //булевы переменные называем с is.. 
-    //         isScroll = false; //от бешенного скрола-тачпада 
-
-    //     var perfomTransition = function (sectionEq) {
-    //         var position = (sectionEq * -100) + '%';
-    //         display.css({
-    //             'transform' : 'translateY(' + position + ')'
-    //         })
-
-    //         section.eq(sectionEq).addClass('activeSection').siblings().removeClass('activeSection');
-
-            
-    //     }
-
-    //     if (deltaY < 0) {//scroll buttom
-    //         console.log('вниз');
-    //         perfomTransition(2);
-    //     }
-
-    //     if(deltaY > 0) {//scroll top
-    //         console.log('вверх');
-    //     }
-
-    //     // console.log(e.originalEvent.deltaY);
-    
-    // });
-
     $(function() {  //DOM content ready
         var section = $('.section'),
-        display = $('.main-content');
+        display = $('.main-content'),
+        isScroll = false; //Флаг от бешенного скрола//is - boolean
 
         //функция премещения секции
         var perfomTransition = function (sectionEq) {
 
-            var position = (sectionEq * -100) + '%';
+            if(!isScroll) { //от бешенного скрола 
+                isScroll = true; //не будет работать пока выполняется действие ниже
+
+                var position = (sectionEq * -100) + '%';
+                
                 display.css({
                     'transform' : 'translateY(' + position + ')'
                 })
+
                 //нашли секцию по номеру.добавили класс.соедям.удалили класс
                 section.eq(sectionEq).addClass('activeSection').siblings().removeClass('activeSection');
+
+                setTimeout(function() {
+                    isScroll = false; // присваиваем обратно как выполнится действие
+                }, 1300);
+            }
         }
 
 
@@ -56,10 +33,13 @@
             var deltaY = e.originalEvent.deltaY;
             var activeSect = section.filter('.activeSection');
             var nextSection = activeSect.next();
+            var prevSection = activeSect.prev();
 
             if (deltaY < 0) {//scroll wheel
                 console.log('страница идёт вниз');
                 console.log(deltaY);
+                //вернёт значение индекса секции следующей перед активной
+                perfomTransition(prevSection.index());
             }
 
             if (deltaY > 0) {//scroll wheel
