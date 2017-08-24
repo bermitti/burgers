373e21a9aace180d6a063293706593f96c415032
bermitti <bermitti@gmail.com>
2017-08-24 20:03:26 +0300

    map1

--- js/app.js
+++ js/app.js
@@ -153,46 +153,6 @@ $('.form__submit').on('submit',function(e){
             }
             ymaps.ready(init);
 
-//one page scroll
-    // $('.wrapper').on('wheel',function(e){
-    //     var deltaY = e.originalEvent.deltaY,
-    //         section = $('.section'),
-    //         activeSect = section.filter('.activeSection'),
-    //         nextSection = activeSect.next(),
-    //         display = $('.main-content');
-    //         //булевы переменные называем с is.. 
-    //         isScroll = false; //от бешенного скрола-тачпада 
-
-    //     var perfomTransition = function (sectionEq) {
-    //         var position = (sectionEq * -100) + '%';
-    //         display.css({
-    //             'transform' : 'translateY(' + position + ')'
-    //         })
-
-    //         section.eq(sectionEq).addClass('activeSection').siblings().removeClass('activeSection');
-
-            
-    //     }
-
-    //     if (deltaY < 0) {//scroll buttom
-    //         console.log('вниз');
-    //         perfomTransition(2);
-    //     }
-
-    //     if(deltaY > 0) {//scroll top
-    //         console.log('вверх');
-    //     }
-
-    //     // console.log(e.originalEvent.deltaY);
-    
-    // });
-
-
-
-
-
-
-
 
 });
 
